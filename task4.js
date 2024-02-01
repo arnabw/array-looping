1993501f99e3ec6a084fbd6c319a1c4f1245c8d8
arnabw <arnabsahawrk@gmail.com>
2024-02-02 01:35:35 +0600

    completed reverse the words of a sentence

---
+++ task4.js
@@ -0,0 +1,8 @@
+// Reverse the words of a sentence. Only the position of the word will be reversed. check out the output.
+
+const statement = "I am a hard working person";
+
+let revStatement = statement.split(" ");
+revStatement.reverse();
+revStatement = revStatement.join(" ");
+console.log(revStatement);
